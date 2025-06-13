e08376f6ec573170dcfc401866ddb3ec66fb7bde
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-06-13 14:06:53 -0300

    Update AlarmModel.java

--- java/AlarmModel.java
+++ java/AlarmModel.java
@@ -18,3 +18,15 @@ public class AlarmModel {
     public boolean deveTocar(String horaAtual) {
         return horaAtual.equals(horaAlarme);
     }
+    public void tocarAlarme() {
+        try {
+            File audioFile = new File("alarm.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
