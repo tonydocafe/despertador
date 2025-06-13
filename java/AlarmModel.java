36eba1709d37d86903356a8d89b874d5c2a8bf36
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-06-13 14:06:40 -0300

    Update AlarmModel.java

--- java/AlarmModel.java
+++ java/AlarmModel.java
@@ -3,3 +3,18 @@ package model;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+public class AlarmModel {
+
+    private String horaAlarme;
+
+    public void setHoraAlarme(String hora) {
+        this.horaAlarme = hora;
+    }
+
+    public String getHoraAlarme() {
+        return this.horaAlarme;
+    }
+
+    public boolean deveTocar(String horaAtual) {
+        return horaAtual.equals(horaAlarme);
+    }
