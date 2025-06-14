694f31d0aa0004f1580b3570c2728d49ecfcce4c
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-06-14 11:25:33 -0300

    Update AlarmView.java

--- java/view/AlarmView.java
+++ java/view/AlarmView.java
@@ -7,3 +7,10 @@ import java.awt.event.ActionListener;
 public class AlarmView extends JFrame {
     private JComboBox<String> horaCombo, minutoCombo, segundoCombo;
     private JButton botao;
+    
+    public AlarmView() {
+        setTitle("Despertador");
+        setSize(300, 120);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
