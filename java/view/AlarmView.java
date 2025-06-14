cb6f6d9ec477a237ae8feabd3706ab6e83e977e2
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-06-14 12:08:30 -0300

    Update AlarmView.java

--- java/view/AlarmView.java
+++ java/view/AlarmView.java
@@ -14,5 +14,12 @@ public class AlarmView extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
         add(new JLabel("Selecione a hora do alarme:"));
+        String[] horas = new String[24];
+        String[] minutosSegundos = new String[60];
+        for (int i = 0; i < 60; i++) {
+            String valor = String.format("%02d", i);
+            if (i < 24) horas[i] = valor;
+            minutosSegundos[i] = valor;
+        }
 
 
