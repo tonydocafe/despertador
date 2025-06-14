24973d540b59b165a250d75d97d86ee267094a72
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-06-14 11:24:39 -0300

    Update AlarmView.java

--- java/view/AlarmView.java
+++ java/view/AlarmView.java
@@ -3,3 +3,7 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+
+public class AlarmView extends JFrame {
+    private JComboBox<String> horaCombo, minutoCombo, segundoCombo;
+    private JButton botao;
